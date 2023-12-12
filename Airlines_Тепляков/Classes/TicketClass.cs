248eecd090dcba9d58225a42e6aae867723e33df
Kirill <kirillteplycov888@gmail.com>
2023-12-12 20:08:43 +0500

    Разработанный класс. Тепляков

---
+++ Airlines_Тепляков/Classes/TicketClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airlines_Тепляков.Classes
+{
+    public class TicketClass
+    {
+        public string price { get; set; }
+        public string from { get; set; }
+        public string to { get; set; }
+        public string time_start { get; set; }
+        public string time_way { get; set;}
+
+        public TicketClass(string price, string from, string to, string time_start, string time_way)
+        {
+            this.price = price;
+            this.from = from;
+            this.to = to;
+            this.time_start = time_start;
+            this.time_way = time_way;
+        }
+    }
+}
