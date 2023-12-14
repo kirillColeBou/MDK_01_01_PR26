4bcb5aa1f7589657cfb232d1527e37179078bd2b
Kirill <kirillteplycov888@gmail.com>
2023-12-14 22:31:33 +0500

    Дополнен элемент Items

--- Airlines_Тепляков/Elements/Items.xaml.cs
+++ Airlines_Тепляков/Elements/Items.xaml.cs
@@ -25,18 +25,26 @@ namespace Airlines_Тепляков.Elements
         {
             InitializeComponent();
             IPrice.Content = item.price + " ₽";
-            fromTime.Content = item.time_start.ToString("HH:mm");
-            IFrom.Content = item.from;
+            fromTime.Content = "Вылет: " + item.time_start.ToString("HH:mm");
+            fromTime_back.Content = "Вылет: " + item.time_start_back.ToString("HH:mm");
+            IFrom_ITo.Content = item.from + "-" + item.to;
             fromDate.Content = item.time_start.ToString("MM.dd.yyyy");
-            toTime.Content = item.time_end.ToString("HH:mm");
-            ITo.Content = item.to;
-            toDate.Content = item.time_end.ToString("MM.dd.yyyy");
+            toTime.Content = "Прилет: " + item.time_end.ToString("HH:mm");
+            toTime_back.Content = "Прилет: " + item.time_end_back.ToString("HH:mm");
+            IFrom_ITo_back.Content = item.to + "-" + item.from;
+            fromDate_back.Content = item.time_end_back.ToString("MM.dd.yyyy");
             TimeSpan tS = item.time_end.Subtract(item.time_start);
+            TimeSpan tS_back = item.time_end_back.Subtract(item.time_start_back);
             string hours = tS.Hours.ToString();
             string minute = tS.Minutes.ToString();
-            if(tS.Hours < 10) hours = "0" + hours;
-            if(tS.Minutes < 10) minute = "0" + minute;
-            time_way.Content = "В пути: " + hours + "ч. " + minute + "мин.";
+            string hours_back = tS_back.Hours.ToString();
+            string minute_back = tS_back.Minutes.ToString();
+            if (tS.Hours < 10) hours = "0" + hours;
+            if (tS.Minutes < 10) minute = "0" + minute;
+            if (tS_back.Hours < 10) hours_back = "0" + hours_back;
+            if (tS_back.Minutes < 10) minute_back = "0" + minute_back;
+            time_way.Content = "В пути (туда): " + hours + "ч. " + minute + "мин.";
+            time_way_back.Content = "В пути (обратно): " + hours_back + "ч. " + minute_back + "мин.";
         }
     }
 }
